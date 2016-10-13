5db5d324927e434560b81c1f023c8659cf5e9144
sayaku <sayaku0210@gmail.com>
2016-10-13 10:45:02 +0800

    v1

--- app/controllers/fb_post_controller.rb
+++ app/controllers/fb_post_controller.rb
@@ -1,9 +1,9 @@
 class FbPostController < ApplicationController
- 
+  before_action :createOAuth
 
   def index
-     session[:oauth] = Koala::Facebook::OAuth.new(Settings.Facebook.facebook_app_id, Settings.Facebook.facebook_secret, post_url)
-	 @auth_url =  session[:oauth].url_for_oauth_code(:permissions => "publish_actions")
+     #session[:oauth] = Koala::Facebook::OAuth.new(Settings.Facebook.facebook_app_id, Settings.Facebook.facebook_secret, post_url)
+	 @auth_url =  @oauth.url_for_oauth_code(:permissions => "publish_actions")
 
 	 redirect_to  @auth_url
 		#puts session.to_s + "<<< session"
@@ -13,11 +13,9 @@ class FbPostController < ApplicationController
   def post
      if params[:code]
   		# acknowledge code and get access token from FB
-  		  @oauth=Koala::Facebook::OAuth.new(Settings.Facebook.facebook_app_id, Settings.Facebook.facebook_secret, post_url)
+  		  #@oauth=Koala::Facebook::OAuth.new(Settings.Facebook.facebook_app_id, Settings.Facebook.facebook_secret, post_url)
 		  @access_token = @oauth.get_access_token(params[:code])
 			
-
-		 # auth established, now do a graph call:
    
       #debugger
       @graph = Koala::Facebook::API.new(@access_token)
@@ -32,15 +30,16 @@ class FbPostController < ApplicationController
 
      
      end
+    
+     redirect_to root_path
 
   end
   
-  def postOver
-  	
+
+
+  def createOAuth
+      @oauth=Koala::Facebook::OAuth.new(Settings.Facebook.facebook_app_id, Settings.Facebook.facebook_secret, post_url)
   end
-
-
-  
 
 
 end
